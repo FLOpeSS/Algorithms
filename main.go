aa52307004f581459df2f02365c25c52435c0864
FLOpeSS <sneto4413@gmail.com>
2023-10-09 13:46:15 -0300

    updating

--- main.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func main() {
-	nums := []int{1, 2, 34, 4}
-	two_sum(nums)
-}
--- twosum.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func two_sum(num []int) {
-	numMap := make(map[int]int)
-	fmt.Println(numMap)
-}
