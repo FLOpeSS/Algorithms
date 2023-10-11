fe06f8ee22b6eb0004d855799e110e169ff77e3b
FLOpeSS <sneto4413@gmail.com>
2023-10-11 13:44:00 -0300

    update

--- trees/bst.go
+++ trees/bst.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+func helloHandler(w http.ResponseWriter) {
+
+}
+
 type nodeTree struct {
 	Data  int
 	Left  *nodeTree
@@ -17,18 +21,36 @@ func newNode(data int) *nodeTree {
 
 }
 
-func printNodes(root *nodeTree) {
-	stack := []*nodeTree{
-		{Data: 1, Left: nil, Right: nil},
-		{Data: 2, Left: nil, Right: nil},
+func recPrint(node *nodeTree) {
+	if node != nil {
+		fmt.Printf("%d", node.Data)
 	}
 
-	random := 40
-	pointer := &random
-	fmt.Println(*pointer)
+	recPrint(node.Right)
+	recPrint(node.Left)
 }
+
+// func printNodes(root *nodeTree) {
+// 	if root == nil {
+// 		return
+// 	}
+//
+// 	stack := []*nodeTree{}
+//
+// 	current := root
+//
+// 	for current != nil || len(stack) > 0 {
+// 		stack = append(stack, current)
+// 		current = current.Left
+// 	}
+//
+// 	current = stack[len(stack)-1]
+// 	stack = stack[:len(stack)-1]
+// }
 
 func main() {
 	node := newNode(5)
-	printNodes(node)
+	node.Left = newNode(6)
+	node.Right = newNode(7)
+	recPrint(node)
 }
